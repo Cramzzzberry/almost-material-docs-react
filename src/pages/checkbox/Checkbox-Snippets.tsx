a9381decfb72d4a437c93aa9e64510e5420ca66a
Cramzzzberry <janroebantuan@gmail.com>
2023-07-03 19:02:08 +0800

    removed label on checkbox

--- src/pages/checkbox/Checkbox-Snippets.tsx
+++ src/pages/checkbox/Checkbox-Snippets.tsx
@@ -1,10 +1,7 @@
 export const CheckboxSnippets = {
   "Checkbox": {
     preview: (
-      <>
       <input className="checkbox" type="checkbox" id="test" />
-      <label htmlFor="test">Checkbox</label>
-      </>
     ),
     htmlCode: "<input class=\"checkbox\" type=\"checkbox\" />",
     jsxCode: "<input className=\"checkbox\" type=\"checkbox\" />",
